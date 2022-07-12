2f5f45082a49fc0a57a56f1cefa85a06beb23d8b
mrbeiley <mbeiley2@gmail.com>
2022-07-12 11:25:08 -0700

    Add a function to Square.

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,13 +1,24 @@
 
+// function placeX (value) { () =>
+//     console.log("PlaceX");
+// }
 
-const Square = () => (
-    <hr
-        style={{
-            height: 50,
-            width: 50,
-            margin: 1
-        }}
-    />
-);
+const placeX = (value) => {
+    console.log(value)
+}
+
+const Square = ({value, color}) => {  
+    return(
+        <button 
+            onClick={placeX(value)}
+            style={{
+                height: 50,
+                width: 50,
+                margin: 1,
+                backgroundColor: color,
+            }}
+        />
+    )
+}
 
 export default Square;
